094ee75c413ccf456b939a4065b3320d984a8785
Mew_miruku <109953842+hinatsu13@users.noreply.github.com>
2023-09-16 21:22:44 +0700

    Update project.c

--- project.c
+++ project.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 
 void how_to_play(char answer[]);
 
 void how_to_play(char answer[])
 {
-    // if ()
+    if (strcmp(tolower(answer), "yes"))
+    {
+        printf("How to play this game!\n");
+        printf("");
+    }
 }
 
 int main()
 {
-    printf("hello world");
+    char how_to[5];
+
+    scanf("Do you want to know how to play this game?\n(Please answer yes/no)\n%s", how_to);
+
+    how_to_play(how_to);
 }
