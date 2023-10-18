f07e073164fe867249d2670f8375b27f910210bf
Mew_miruku <109953842+hinatsu13@users.noreply.github.com>
2023-10-18 22:16:38 +0700

    เสร็จทันที

--- project.c
+++ project.c
@@ -12,6 +12,7 @@
 
 void how_to_play(char answer);
 int random_num();
+void delay(int number_of_seconds);
 void mark_choose(int number, char set[]);
 int check_choose(int num, int number[], int round);
 int winner(int winscore1, int winscore2, int totalnumber1, int totalnumber2);
@@ -27,6 +28,14 @@ void how_to_play(char answer)
         printf("And, of course, we have a special rule. At the beginning of the game, there will be a random ability assigned to each player to help increase your number or decrease the opponent's numbers.");
         printf("\n-------\n");
     }
+}
+
+void delay(int number_of_seconds)
+{
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds)
+        ;
 }
 
 int random_num()
@@ -105,7 +114,7 @@ int main()
     char how_to, name1[10], name2[10], ans, ok[100];
     int num[6], number[10];
     int round = 0;
-    char *ability[2] = {"Increase your score", "Deducting points from the opposing player"};
+    char *ability[3] = {"Increase your score", "Deducting points from the opposing player", "Increase your win score but random player"};
     int select1, select2, use_abi_p1 = 0, use_abi_p2 = 0;
     char confirm1, confirm2;
     int totalnumber1 = 0, totalnumber2 = 0;
@@ -123,9 +132,9 @@ int main()
     scanf(" %[^\n]", name2);
 
     printf("It's time to random abilities!\n");
-    int ability1 = random_num() % 2;
+    int ability1 = random_num() % 3;
     int point_abi1 = (random_num() % 4) + 1;
-    int ability2 = (random_num() + 1) % 2;
+    int ability2 = (random_num() + 1) % 3;
     int point_abi2 = (random_num() % 4) + 1;
 
     printf("From now on, no other player should see your abilities. Ready or not?\n");
@@ -133,12 +142,16 @@ int main()
     scanf(" %[^\n]", ok);
     system(CLEAR);
 
-    printf("%s, your ability is %s (%d)\n", name1, ability[ability1], point_abi1);
+    if (strcmp(ability[ability1], "Increase your win score but random player") == 0)
+        printf("%s, your ability is %s\n", name1, ability[ability1]);
+    else printf("%s, your ability is %s (%d)\n", name1, ability[ability1], point_abi1);
     printf("If you've memorized the abilities, please type y\n");
     scanf(" %c", &confirm1);
     system(CLEAR);
 
-    printf("%s, your ability is %s (%d)\n", name2, ability[ability2], point_abi2);
+    if (strcmp(ability[ability2], "Increase your win score but random player") == 0)
+        printf("%s, your ability is %s\n", name1, ability[ability2]);
+    else printf("%s, your ability is %s (%d)\n", name2, ability[ability2], point_abi2);
     printf("If you've memorized the abilities, please type y\n");
     scanf(" %c", &confirm2);
     system(CLEAR);
@@ -161,7 +174,9 @@ int main()
             printf("%s (win %d) score: %d\n", name1, winscore1, totalnumber1);
             printf("%s (win %d) score: %d\n", name2, winscore2, totalnumber2);
             printf("*****************\n");
-            printf("%s\n", set_of_num);
+            for (int l=0; l<41; l++)
+                printf("%c", set_of_num[l]);
+            printf("\n");
             printf("%s, please enter the number you choose.\n", name1);
             scanf("%d", &select1);
             int num1 = check_choose(select1, num, round);
@@ -174,7 +189,9 @@ int main()
             printf("%s (win %d) score: %d\n", name1, winscore1, totalnumber1);
             printf("%s (win %d) score: %d\n", name2, winscore2, totalnumber2);
             printf("*****************\n");
-            printf("%s\n", set_of_num);
+            for (int l=0; l<41; l++)
+                printf("%c", set_of_num[l]);
+            printf("\n");
             printf("%s, please enter the number you choose.\n", name2);
             scanf("%d", &select2);
             int num2 = check_choose(select2, num, round);
@@ -202,6 +219,27 @@ int main()
                     totalnumber1 += point_abi1;
                 else if (strcmp(ability[ability1], "Deducting points from the opposing player") == 0)
                     totalnumber2 -= point_abi1;
+                else
+                {
+                    printf("Time to roll!!!\n3\n");
+                    int who_get = random_num() % 2;
+                    delay(1);
+                    printf("2\n");
+                    delay(1);
+                    printf("1\n");
+                    delay(1);
+                    if (who_get == 0)
+                    {
+                        printf("Congratulations to %s! Your win score +1.\n", name1);
+                        winscore1++;
+                    }
+                    else if (who_get == 1)
+                    {
+                        printf("Congratulations to %s! Your win score +1.\n", name2);
+                        winscore2++;
+                    }
+                    
+                }
                 use_abi_p1++;
             }
         }
@@ -225,6 +263,27 @@ int main()
                     totalnumber2 += point_abi2;
                 else if (strcmp(ability[ability2], "Deducting points from the opposing player") == 0)
                     totalnumber1 -= point_abi2;
+                else
+                {
+                    printf("Time to roll!!!\n3\n");
+                    int who_get = random_num() % 2;
+                    delay(1);
+                    printf("2\n");
+                    delay(1);
+                    printf("1\n");
+                    delay(1);
+                    if (who_get == 0)
+                    {
+                        printf("Congratulations to %s! Your win score +1.\n", name1);
+                        winscore1++;
+                    }
+                    else if (who_get == 1)
+                    {
+                        printf("Congratulations to %s! Your win score +1.\n", name2);
+                        winscore2++;
+                    }
+                    
+                }
                 use_abi_p2++;
             }
         }
